2c744d664a280c8e6112a1da797d96c1bf6d2641
zva-2000 <135205647+zva-2000@users.noreply.github.com>
2023-06-13 08:33:47 +0600

    Add files via upload

---
+++ src/components/ChangeInput.vue
@@ -0,0 +1,47 @@
+<template>
+
+<input type="text" v-model="note.title" v-if="index === curIndex">
+
+<p v-bind="newTitle" @click="toChange" style="cursor: pointer" v-else>{{ note.title }}</p>
+
+<button class="btn btnPrimary" @click="toChange">Change</button>
+
+</template>
+
+<script>
+
+export default {
+    data() {
+    return {
+        changeN: true,
+    }
+    },
+    props: {
+    note: {
+      type: Object,
+      required: true
+    },
+      index: {
+      type: Number,
+      required: true
+      },
+      notes: {
+        type: Array,
+        required: true
+     },
+    },
+    methods: {
+        toChange () {
+            this.changeN = !this.changeN
+            this.$emit('toChange', this.index)
+        }
+    }
+  }
+
+
+
+</script>
+
+<style lang="scss">
+
+</style>
